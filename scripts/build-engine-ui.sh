4fdb9bca72f9ec9849b831548b050c58f6b723f3
Shaoke <shaokexu@gmail.com>
2019-11-12 00:35:00 -0800

    Add build-engine-ui script

---
+++ scripts/build-engine-ui.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+###########################
+# Build dia engine with ui
+###########################
+# Available Envs: 
+# 1. BRANCH_UI: git branch for `dia-ui`. Default is `develop`
+# 2. BRANCH_ENGINE: git branch for `dia-engine`. Default is `develop`
+# 3. DIST: which folder to store build files. Default is `dist-engine-ui`
+
+if [[ -z "${BRANCH_UI}" ]]; then
+  BRANCH_UI="develop"
+fi
+
+if [[ -z "${BRANCH_ENGINE}" ]]; then
+  BRANCH_ENGINE="develop"
+fi
+
+if [[ -z "${DIST}" ]]; then
+  DIST="dist-engine-ui"
+fi
+
+# Remove previous build
+rm -rf ${DIST}
+mkdir ${DIST}
+
+###########################
+echo "Start build dia-engine..."
+cd dia-engine
+echo "Current Folder: " && pwd
+git checkout ${BRANCH_ENGINE}
+git pull
+npm install
+npm run build
+cp -rf build/ ../${DIST}
+cp package.json ../${DIST}
+echo "BUild dia-engine successfully"
+
+###########################
+echo "Start build dia-ui..."
+cd ../dia-ui
+echo "Current Folder: " && pwd
+git checkout ${BRANCH_UI}
+git pull
+npm install
+npm run build
+cp -rf dist/ ../${DIST}/public
+echo "BUild dia-ui successfully"
+
+###########################
+echo "Start Server"
+cd ../${DIST}
+npm install --production
+node index.js

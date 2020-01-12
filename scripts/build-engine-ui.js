3946fcf5918391d15f644439469d6e7308c22bb4
Shaoke <shaokexu@gmail.com>
2020-01-11 17:27:21 -0800

    fixed issue-27: Cannot generate package in windows.

--- scripts/build-engine-ui.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const BRANCH_UI = process.env.BRANCH_UI||'develop';
-const BRANCH_ENGINE = process.env.BRANCH_ENGINE||'develop';
-const DIST = process.env.DIST||'build/';
-const ENGINE_UI_FOLDER_NAME = process.env.ENGINE_UI_FOLDER_NAME||'engine-ui/';
-const TARGET_PATH = path.join(DIST, ENGINE_UI_FOLDER_NAME);
-
-console.log(`TARGET_PATH: ${TARGET_PATH}`);
